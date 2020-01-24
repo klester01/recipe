e4d55e1606329ac0a0c44753620baca925ed7bd4
klester01 <klester01@aol.com>
2020-01-23 21:06:02 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function marinaraSauce(cookName) {
+var two = 2;
+var oneFourth = "1/4";
+console.log("Hey, "+cookName+"! This is my marinara sauce recipe")
+console.log("Heat a medium-large saucepan over medium heat.");
+console.log("Add "+two+" teaspoon of oil and 5 garlic cloves, cook until golden, about 2 minutes");
+console.log("Add "+oneFourth+" cup of Water, "+two+" cans of crush tomatoes and salt and season with black pepper to taste");
+console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+console.log ("Stir in "+oneFourth+" cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+marinaraSauce("Kel");
+
+
+
